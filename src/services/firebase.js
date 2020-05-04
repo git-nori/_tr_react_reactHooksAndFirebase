8c1672e6b556bf635d82f53445789ce69dec0ce4
HironoriSato <maza.hs5360@gmail.com>
2020-05-04 17:52:10 +0900

    [add]firebaseのsetting

---
+++ src/services/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase'
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyC60VrFnQYE9N94YCWXAFQ8NBoPq3aE3-8",
+  authDomain: "chatty-6c49e.firebaseapp.com",
+  databaseURL: "https://chatty-6c49e.firebaseio.com",
+  projectId: "chatty-6c49e",
+  storageBucket: "chatty-6c49e.appspot.com",
+  messagingSenderId: "67193945759",
+  appId: "1:67193945759:web:5f7d45eae5bfacba8dacaa"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth
+export const db = firebase.database()
