c82712b9e0b69924772a89531a2388b65c715772
HironoriSato <maza.hs5360@gmail.com>
2020-05-04 20:45:02 +0900

    [add]Login画面を実装

---
+++ src/features/Login/Forms.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+import { Form, Button, Alert } from 'react-bootstrap'
+
+import { signin } from '../../helpers/auth'
+
+const Forms = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  })
+  const [error, setError] = useState(null)
+
+  const handleChage = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value })
+  }
+
+  const handleSubmit = async () => {
+      setError(null)
+      try {
+        await signin(formData.email, formData.password)
+      } catch (err) {
+        setError(err.message)
+      }
+  }
+
+  const renderAlert = () => {
+    if (error) {
+      return <Alert variant="danger">{error}</Alert>
+    }
+  }
+
+  return (
+    <Form>
+      {renderAlert()}
+      <p>Fill in the form below to create an account.</p>
+      <Form.Group controlId="email">
+        <Form.Label>Email</Form.Label>
+        <Form.Control
+          placeholder="Email"
+          type="email"
+          onChange={handleChage}
+          value={formData.email}>
+        </Form.Control>
+      </Form.Group>
+      <Form.Group controlId="password">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          placeholder="Password"
+          type="password"
+          onChange={handleChage}
+          value={formData.password}>
+        </Form.Control>
+      </Form.Group>
+      <Button variant="outline-primary" onClick={handleSubmit}>Login</Button>
+      <hr />
+
+      <p>Dont have an account? <Link to="/signup">Signup</Link></p>
+    </Form>
+  )
+}
+
+export default Forms
--- src/features/Login/LoginPage.jsx
+++ src/features/Login/LoginPage.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import Forms from './Forms'
+
+import { Container } from 'react-bootstrap'
 
 const LoginPage = () => {
   return (
-    <h1>LoginPage</h1>
+    <Container>
+      <h1>Login to <Link to="/">Chatty</Link></h1>
+      <Forms />
+    </Container>
   )
 }
 
