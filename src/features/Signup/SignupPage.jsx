b79f3e0b32bac2117959321f68027783ea86705d
HironoriSato <maza.hs5360@gmail.com>
2020-05-04 20:24:46 +0900

    [add]Signup画面を実装

--- src/features/Signup/SignupPage.jsx
+++ src/features/Signup/SignupPage.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import Forms from './Forms'
+
+import { Container } from 'react-bootstrap'
 
 const SignupPage = () => {
   return (
-    <h1>SignupPage</h1>
+    <Container>
+      <h1>Sign up to <Link to="/">Chatty</Link></h1>
+      <Forms />
+    </Container>
   )
 }
 
